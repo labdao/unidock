59fcedc1395905e70d974d700c2b23da7db39c23
Seth Howes <sethshowes@gmail.com>
2023-10-18 17:21:08 +0000

    added output parsing function

--- src/main.py
+++ src/main.py
@@ -1,14 +1,45 @@
 """Main file for the project."""
 import subprocess
 import os
+import pickle
 
 valid_params = [
-    "receptor", "flex", "ligand", "ligand_index", "batch", "gpu_batch", "scoring",
-    "maps", "center_x", "center_y", "center_z", "size_x", "size_y", "size_z", "autobox",
-    "out", "dir", "write_maps", "cpu", "seed", "exhaustiveness", "max_evals", "num_modes",
-    "min_rmsd", "energy_range", "spacing", "verbosity", "max_step", "refine_step", "max_gpu_memory",
-    "search_mode", "config", "help", "version"
+    "receptor",
+    "flex",
+    "ligand",
+    "ligand_index",
+    "batch",
+    "gpu_batch",
+    "scoring",
+    "maps",
+    "center_x",
+    "center_y",
+    "center_z",
+    "size_x",
+    "size_y",
+    "size_z",
+    "autobox",
+    "out",
+    "dir",
+    "write_maps",
+    "cpu",
+    "seed",
+    "exhaustiveness",
+    "max_evals",
+    "num_modes",
+    "min_rmsd",
+    "energy_range",
+    "spacing",
+    "verbosity",
+    "max_step",
+    "refine_step",
+    "max_gpu_memory",
+    "search_mode",
+    "config",
+    "help",
+    "version",
 ]
+
 
 def run_unidock(**kwargs):
     """Runs Uni-Dock."""
@@ -16,7 +47,7 @@ def run_unidock(**kwargs):
     for param in kwargs:
         if param not in valid_params:
             raise ValueError(f"Invalid parameter: {param}")
-    
+
     # Get all ligand files from the input directory
     if "gpu_batch" in kwargs:
         ligand_dir = kwargs["gpu_batch"]
@@ -26,20 +57,79 @@ def run_unidock(**kwargs):
     # Construct the command
     command = ["unidock"]
     for param, value in kwargs.items():
-        command.append(f"--{param}")
-        command.append(str(value))
+        # Add the list of filepaths if the parameter is "gpu_batch"
+        if param == "gpu_batch":
+            command.append("--gpu_batch")
+            command.extend(ligand_files)
+        else:
+            command.append(f"--{param}")
+            command.append(str(value))
 
     # Run the command
     subprocess.check_call(command)
 
 
+def parse_outputs(output_dir):
+    """Processes the pdbqt outputs into a list of dictionaries."""
+    # Get all ligand files from the output directory
+    ligand_files = [os.path.join(output_dir, f) for f in os.listdir(output_dir)]
+    # Variables to extract
+    key_var_names = ["INTER + INTRA", "INTER", "INTRA", "UNBOUND", "NAME"]
+    # List of lists of dictionaries containing the results for each ligand model
+    ligand_results = []
+
+    # Go through each ligand
+    for f in ligand_files:
+        with open(f, "r", encoding="utf-8") as file:
+            lines = file.readlines()
+            # Identify the indices of the lines starting with "MODEL"
+            model_line_indices = [
+                i for i, line in enumerate(lines) if line.startswith("MODEL")
+            ]
+            # List to store the results for each ligand model
+            model_results = []
+
+            # Extract key information from each model
+            for i in model_line_indices:
+                res = lines[i + 1 : i + 6]
+                # Extracts the values from the key information and whitespace
+                res = [line.split(":")[1].strip() for line in res]
+                # Remove the second and third results for the first item
+                res[0] = res[0].split()[0]
+                # Add the ligand name
+                res.append(lines[i + 6].split("=")[1].strip())
+                # Create a dictionary of the results
+                res_dict = dict(zip(key_var_names, res))
+                # Add the dictionary to the list of results
+                model_results.append(res_dict)
+
+        # Add the results for all models for the given ligand
+        ligand_results.append(model_results)
+
+    return ligand_results
+
+
 def main():
     """Main function for the project."""
     # Run Uni-Dock
-    run_unidock(receptor="data/inputs/mmp13.pdbqt", gpu_batch="data/inputs/ligands", dir="data/outputs",
-                center_x=-36.0095, center_y=25.628500000000003, center_z=67.49199999999999,
-                size_x=17.201, size_y=14.375000000000004, size_z=12.239999999999995
+    run_unidock(receptor="data/inputs/mmp13.pdbqt",
+                gpu_batch="data/inputs/ligands",
+                dir="data/outputs",
+                center_x=-6.9315,
+                center_y=26.579,
+                center_z=54.135999999999996,
+                size_x=15.341000000000001,
+                size_y=10.828,
+                size_z=17.556000000000004,
+                search_mode="fast"
                 )
+
+    # Parse the outputs
+    results = parse_outputs("data/outputs")
+
+    # Save the results to a pickle file
+    with open("data/results.pkl", "wb") as file:
+        pickle.dump(results, file)
 
 
 if __name__ == "__main__":
